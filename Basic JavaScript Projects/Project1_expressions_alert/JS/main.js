3838a8323bec158f9a225cbcaae79b7fc38a6be2
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-20 21:09:19 -0500

    Update main.js
    added more document.write lines for special characters and to concatenate

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,9 @@
-var A;
-A = 'This is a string.';
+var A = 'This is a string.';
 window.alert(A);
 document.write(A);
-document.write(' Use backslash \\ to special character\(s\) like \' \" \( \) and others.')
+document.write(' Use backslash \\ to special character\(s\) like \' \" \( \) and others.');
+document.write(' Writing a string here'
++' to use the \+ to concatenate'
++' strings together.');
+var B = ' Concatenated' + ' String'
+document.write(B);
