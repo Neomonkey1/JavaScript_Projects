95df509731e98b4a6161da770653cb340056bff3
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-21 10:10:52 -0500

    Update main.js
    modified myFunction to concatenate my var A and B

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,8 @@
 var A = 'This is var A';
-var B = 'This is var B';
+var B = ' This is var B';
 function myFunction(){// This function will display the string from var A and var B
-    document.getElementById("demo1").innerHTML = A;
-    document.getElementById("demo2").innerHTML = B;
+    var sentence = A;
+    sentence += B;
+    document.getElementById("demo1").innerHTML = sentence;
+    
 }
