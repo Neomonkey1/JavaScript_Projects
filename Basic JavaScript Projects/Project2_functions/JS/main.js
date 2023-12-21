d120280249b607a5dbaf00a7c4b4d7774b46fdb5
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-21 10:12:01 -0500

    Update main.js
    added comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,7 +1,7 @@
 var A = 'This is var A';
 var B = ' This is var B';
 function myFunction(){// This function will display the string from var A and var B
-    var sentence = A;
+    var sentence = A;// modified function to concatenate var A and B
     sentence += B;
     document.getElementById("demo1").innerHTML = sentence;
     
