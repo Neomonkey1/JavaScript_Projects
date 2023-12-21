d7001ce3bc4ddfa4af95521881d17589a27b90cd
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-20 21:52:38 -0500

    Update main.js
    added comments using //

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,14 +1,14 @@
-window.alert('Hello, World!');
-var A = 'This is a string.';
-window.alert(A);
-document.write(A);
-document.write(' Use backslash \\ to special character\(s\) like \' \" \( \) and others.');
-document.write(' Writing a string here'
+window.alert('Hello, World!');//This is the first pop up window on the page.
+var A = 'This is a string.';//assigned the varible 'A' with a string 
+window.alert(A); //This is the second pop up window, it usings the varible 'A'
+document.write(A);//This writes on the page and uses the varible 'A'
+document.write(' Use backslash \\ to special character\(s\) like \' \" \( \) and others.');//This writes on the page and shows how to use the backslash \ 
+document.write(' Writing a string here'//These use the + to concatenate (bring together) multiple lines
 +' to use the \+ to concatenate'
 +' strings together.');
-var B = ' Concatenated' + ' String.'
-document.write(B);
-var Family = ' The Simpsons', Dad = ' Homer ', Mom = ' Marge ',
+var B = ' Concatenated' + ' String.';// assigned the varible 'B' a concatenated string
+document.write(B);//This writes on the page varible 'B'
+var Family = ' The Simpsons', Dad = ' Homer ', Mom = ' Marge ',//assigns the varible 'Family' with multiple varibles 
 Son = ' Bart ', Daughter = ' Lisa '; 
-document.write(Daughter);
-document.write(5+5);
+document.write(Daughter);//This calls one of the varibles from 'Family'
+document.write(5+5);//This statement (document.write) contains and prints an expression.
