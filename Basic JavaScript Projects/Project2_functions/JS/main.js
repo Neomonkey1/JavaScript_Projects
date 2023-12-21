8ba934cba50b50186162ad21679e73a819a68eb7
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-21 09:57:04 -0500

    Create main.js
    made var A and B
    made myFunction to displayed when called
    added comments

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+var A = 'This is var A';
+var B = 'This is var B';
+function myFunction(){// This function will display the string from var A and var B
+    document.getElementById("demo1").innerHTML = A;
+    document.getElementById("demo2").innerHTML = B;
+}
