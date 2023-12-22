86e424f7d56ed40c9c266aa081573e7f1135eaac
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-21 22:23:29 -0500

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -4,5 +4,9 @@ function myFunction(){// This function will display the string from var A and va
     var sentence = A;// modified function to concatenate var A and B
     sentence += B;
     document.getElementById("demo1").innerHTML = sentence;
-    
+} 
+function myFunction2(p1, p2) {
+    return p1 * p2;
 }
+let result = myFunction2(10, 5);
+document.getElementById("demo2").innerHTML = result;
