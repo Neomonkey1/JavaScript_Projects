890b7d9e1353287d2297e6a286674cdd09ddb463
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-22 22:11:28 -0500

    Update main.js
    added identical keys to see what happens added comments
    used the delete operator added comments

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,16 @@
-function my_Dictionary(){
+function my_Dictionary(){//made function for basic dictionary
     var Animal = {
         Species: "Dog",
         Color: "Black",
         Breed: "Labrador",
         Age: 5,
-        Sound: 'Bark!'
+        Sound: 'Bark!',
+        Species: "Cat",//made KVPs with identical keys
+        Color: "White",//later key values show over first 
+        Breed: "Tabby",
+        Age: 7,
+        Sound: "Meow!"
     };
+    delete Animal.Sound;//delete operator deletes key .Sound
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 }
