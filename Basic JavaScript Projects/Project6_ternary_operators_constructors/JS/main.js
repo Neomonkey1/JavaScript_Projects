8d328bcf4d789b8a8ed802996e1882651b2e6d6d
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-26 22:44:04 -0500

    Update main.js
    added two functions
    object constructor
    myfunction to display Erik's vehicle info

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -10,3 +10,17 @@ function Vote_Function(){//Function to see if person is old enough to vote.
     Can_Vote = (Age < 18) ? 'You are not old enough to': 'You can';
     document.getElementById('Vote').innerHTML = Can_Vote + ' Vote!';
 }
+function Vehicle(Make, Model, Year, Color){//This function makes an object constructor
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle('Dodge', 'Viper', 2020, 'Red');
+var Emily = new Vehicle('Jeep', 'Trail Hawk', 2019, 'White and Black');
+var Erik = new Vehicle('Ford', 'Pinto', 1971, 'Mustard');
+function myFunction(){//This calls my function to display Erik's Vehicle values.
+    document.getElementById('Keywords_and_Constructors').innerHTML =
+    'Erik drives a '+ Erik.Vehicle_Color + '-colored '+ Erik.Vehicle_Model +
+    ' manufactured in '+ Erik.Vehicle_Year;
+}
