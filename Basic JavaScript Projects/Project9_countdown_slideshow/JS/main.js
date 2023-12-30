29ed785005003d13eb715f014c40de7df2f16794
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2023-12-29 21:04:38 -0500

    Update main.js
    added function to images to make into a slideshow.

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -13,3 +13,29 @@ function countdown(){//Made function countdown
     }
     tick();//calls function tick
 }
+//starting slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+// Next and previous controls
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+function showSlides (n){
+    let i;
+    let slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('dot');
+    if (n > slides.length){slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display= 'none';
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
