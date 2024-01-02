6a8ab63b745a76cf4293e00be21e1eb5e86c8366
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-01-01 21:09:44 -0500

    Update main.js
    added function for array
    added function for constant
    added comments
    changed value of constant
    added comments

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -18,7 +18,7 @@ var Instruments = ['Guitar', 'Drums', 'Piano', 'Bass', 'Violin',
 'Trumpet','Flute'];
 var Content = '';
 var Y;
-function for_Loop(){//function for_Loop
+function for_Loop(){//made function for_Loop
     //start with Y being 0, while Y is less than Instruments
     //length, increment Y by 1 tell the for loop is done.
     for (Y = 0; Y < Instruments.length; Y++){
@@ -28,4 +28,26 @@ function for_Loop(){//function for_Loop
         Content += Instruments[Y] + '<br>';
     }//This will display the strings in HTML
     document.getElementById('List_of_Instruments').innerHTML = Content;
+}
+function array_Function(){//made function cat_pics
+    //making an array Cat_Picture blank then filling in the array
+    var Cat_Picture = [];
+    Cat_Picture[0] = 'sleeping';
+    Cat_Picture[1] = 'playing';
+    Cat_Picture[2] = 'eating';
+    Cat_Picture[3] = 'purring';
+    //displaying a string and adding element index 2 from array to display in string
+    document.getElementById('Array').innerHTML = 'In this picture, the cat is '+
+    Cat_Picture[2] + '.';
+}
+function constant_function(){//made function for constant
+    const Musical_Instrument = {type: 'guitar', brand: 'Fender', color: 'black'};
+    Musical_Instrument.color = 'green';//changed from 'blue' to 'green'
+    Musical_Instrument.price = '$900';
+    //adding property and value
+    Musical_Instrument.brand = 'Ibanez'
+    //charged output to new property and value
+    document.getElementById('Constant').innerHTML = 'The cost of the ' +
+    Musical_Instrument.color + ' ' + Musical_Instrument.brand +' '+ Musical_Instrument.type + 
+    ' was ' + Musical_Instrument.price;
 }
